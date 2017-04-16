4322539ec529e288c9c66484d46fa7639876e865
Dan <dan.j.siegel@gmail.com>
2017-04-16 14:45:01 -0700

    fixed input from file

--- projects/project2/project2.cpp
+++ projects/project2/project2.cpp
@@ -14,28 +14,37 @@ struct book {
 		int holds;
 	};
 
-
+int lineRecognition(char getLineData[250]){
+	int lineDataAsInt;	
+	istringstream iss(getLineData);
+	iss >> lineDataAsInt;
+	return lineDataAsInt;
+}
 
 int main() {
 	int numOfBooks, action, tempID;
 	char line[250];
 	ifstream bookList("library.txt");
 
-	bookList >> numOfBooks;
+	bookList.getline(line, 250, '\n');
+	istringstream iss(line);
+	iss >> numOfBooks; 
 	struct book s[numOfBooks];//reading from lines is not triggering next line. Need to move bookList >> s[i].id to a getline and put bach in as sstream. 
+
+
 	for (int i = 0; i < numOfBooks; i++){
 		bookList.getline(line, 250, '\n');
-		istringstream iss(line);
-		iss >> s[i].id;
-		cout << s[i].id << endl;
+		s[i].id = lineRecognition(line);
 		bookList.getline(s[i].title, 250, '\n');
-		cout << s[i].title << endl;
-		bookList.getline(s[i].author,250, 'n');
-		bookList >> s[i].copies;
-		bookList >> s[i].checkOuts;
-		bookList >> s[i].holds;	
+		bookList.getline(s[i].author, 250, '\n');
+		bookList.getline(line, 250, '\n');//copies
+		s[i].copies = lineRecognition(line);
+		bookList.getline(line, 250, '\n');//checkouts
+		s[i].checkOuts = lineRecognition(line);
+		bookList.getline(line, 250, '\n');//holds
+		s[i].holds = lineRecognition(line);
 	}
- /*
+/*
 	do {i
 		cout <<"*"  << "Main Menu" << "*" << endl;
 		cout << "1 - Print Catalog\n" << "2 - Search by Title\n" << "3 - Search by Author\n" << "4 - Do Action\n" << "5 - Quit\n" << "Enter choice: " << endl;
