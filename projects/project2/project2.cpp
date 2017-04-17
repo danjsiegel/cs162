bc48bb1295fa65e1162837ae657f14f1c71db69f
danjsiegel <dan.j.siegel@gmail.com>
2017-04-16 17:51:37 -0700

    completed logic to compare titles

--- projects/project2/project2.cpp
+++ projects/project2/project2.cpp
@@ -22,8 +22,9 @@ int lineRecognition(char getLineData[250]){
 }
 
 int main() {
-	int numOfBooks, action, tempID;
-	char line[250];
+	int numOfBooks, action, tempID, foundIteration;
+	char line[250], compare[250];
+	bool found = false;
 	ifstream bookList("library.txt");
 
 	bookList.getline(line, 250, '\n');
@@ -44,38 +45,71 @@ int main() {
 		bookList.getline(line, 250, '\n');//holds
 		s[i].holds = lineRecognition(line);
 	}
-/*
-	do {i
-		cout <<"*"  << "Main Menu" << "*" << endl;
-		cout << "1 - Print Catalog\n" << "2 - Search by Title\n" << "3 - Search by Author\n" << "4 - Do Action\n" << "5 - Quit\n" << "Enter choice: " << endl;
-		cin >> action;
 
+	do {
+		cout << "\n*********"  << "Main Menu" << "*********" << endl;
+		cout << "1 - Print Catalog\n" << "2 - Search by Title\n" << "3 - Search by Author\n" << "4 - Do Action\n" << "5 - Quit\n" << "Enter choice: " << endl;
+		
+		cin >> action;
 		switch (action){
 		case 1:
-			cout << "case1" << endl;
+			cout << "%%%%%%% Book Catalog %%%%%%%" << endl;
+			for (int i = 0; i < numOfBooks; i++){
+				cout << "-----" << s[i].id << "-----" << endl;							
+				cout << "Title: " << s[i].title << endl;
+				cout << "Author: " << s[i].author << endl;
+				cout << "Number of Copies: " << s[i].copies << endl;
+				cout << "Number Checked out: " << s[i].checkOuts << endl;
+				cout << "Number of Holds: " << s[i].holds << endl;				
+			}
 			break;
 		case 2:
-			cout << "case2" <<endl;
+			cin.clear();
+			cin.ignore();
+			found = false; 
+			cout << "Title of Book? " <<endl;
+			cin.getline(compare, 250, '\n');
+			for (int i = 0; i < numOfBooks; i++){
+				int equal;				
+				equal = strcmp(compare, s[i].title);
+				cout << s[i].title << endl;
+				cout << compare << endl;
+				if (equal == 0){
+				found = true;
+				foundIteration = i;
+				}
+			}
+			if (found == true){
+				cout << "-----" << s[foundIteration].id << "-----" << endl;							
+				cout << "Title: " << s[foundIteration].title << endl;
+				cout << "Author: " << s[foundIteration].author << endl;
+				cout << "Number of Copies: " << s[foundIteration].copies << endl;
+				cout << "Number Checked out: " << s[foundIteration].checkOuts << endl;
+				cout << "Number of Holds: " << s[foundIteration].holds << endl;	
+			} else {
+				cout << "Book Not Found" << endl;
+			}
 			break;
 		case 3:
+			cin.clear();
+			cin.ignore();
 			cout << "case3" << endl;
 			break;
 		case 4:
 			cout << "case4" << endl;
 			break;
-		default:
-			cout << "default" << endl;
+		case 5: 
+			cout << "goodbye" << endl;
 			break;
-		}
-		if ((action > 5) || (!action) || (action < 1)){
+		default:
 			cout << "Invalid selection" << endl;
 			cin.clear();
-			cin.ignore(100, '\n'); 	
+			cin.ignore(100, '\n'); 
+			break;
 		}
-
 	} while (action != 5);
 
-*/	
+
 	bookList.close();
 	return 0;
 }
