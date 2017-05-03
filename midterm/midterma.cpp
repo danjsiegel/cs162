4e3e7e23e4e577b9ddb2b0ff3c83f1b5dc63c125
dan siegel <dan.j.siegel@gmail.com>
2017-05-02 18:13:00 -0700

    raw midterm file

---
+++ midterm/midterma.cpp
@@ -0,0 +1,161 @@
+/* Midterm proctor exam version A
+ * 
+ * INSTRUCTIONS:  Write two functions in the space 
+ * indicated below.
+ * 
+ * printArray function:  This should take an array of ints 
+ * and a count.  It should then print out the array of numbers
+ * one number per line.
+ * 
+ * remove function:  This should take an array, a number
+ * to find and an amount. It will then remove the elements
+ * up to the given amount starting after the first
+ * instance of the given number. If the given number is not
+ * found, it should do nothing.
+ * It should move elements to fill in where the removed 
+ * elements were located (see examples below).
+ * Assume that the amount is less than or equal to the
+ * number of elements after the found number. 
+ * 
+ * You can change this file all you want during
+ * the exam but keep in mind that I will ONLY BE
+ * USING YOUR FUNCTIONS by copying them to another
+ * main.cpp file (with different variable values)
+ * for grading. So don't code to the given data values.
+ * 
+ * Do not use global variables/constants
+ * or goto's in your code.  Please only make two
+ * functions (there's really no need for more than two).
+ * 
+ * Don't ask the user for information.  The main
+ * function will get all information needed and pass it
+ * into your functions (see main function for
+ * how your functions will be called).
+ * 
+ * 
+ * three example runs are below:
+ Original Array
+-----------
+10
+32
+3
+41
+60
+7
+18
+77
+3
+5
+90
+----------------
+remove after what number?3
+number of elements to remove?3
+After Removal
+-----------
+10
+32
+3
+18
+77
+3
+5
+90
+----------------
+Original Array
+-----------
+10
+32
+3
+41
+60
+7
+18
+77
+3
+5
+90
+----------------
+remove after what number?101
+number of elements to remove?3
+After Removal
+-----------
+10
+32
+3
+41
+60
+7
+18
+77
+3
+5
+90
+----------------
+Original Array
+-----------
+10
+32
+3
+41
+60
+7
+18
+77
+3
+5
+90
+----------------
+remove after what number?60
+number of elements to remove?6
+After Removal
+-----------
+10
+32
+3
+41
+60
+----------------
+
+
+
+ * 
+ */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// PLEASE PUT YOUR FUNCTIONS BELOW THIS LINE
+
+
+
+//  END OF FUNCTIONS
+
+
+
+int main()
+{
+    int array[200] = {10,32,3,41,60,7,18,77,3,5,90};
+    int count = 11;
+	int find, num;
+	
+    cout << "Original Array" << endl;
+    cout << "-----------" << endl;
+	printArray(array, count);
+    cout << "----------------" << endl;
+	           
+	cout << "remove after what number?";
+	cin >> find;
+    cout << "number of elements to remove?";
+    cin >> num;
+	
+    remove(find, num, array, count);
+	
+    cout << "After Removal" << endl;
+    cout << "-----------" << endl;
+	printArray(array, count);
+    cout << "----------------" << endl;
+	
+	return 0;
+}
